9cedc8caf1459bd26fec222ffe1a3a7c0b337479
BarrelProd <loftbyadam@gmail.com>
2023-02-13 12:39:39 +0100

    BossFightBegining

---
+++ PWeekProject/Assets/Scripts/FireBreathAttack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBreathAttack : MonoBehaviour
+{
+    public Transform spawnPosition;
+    public GameObject fireAttack;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
