8c33b2f33ca121c9717e7cf06a6d7c4f8f7a21e2
Okiman42 <antonaxel05@gmail.com>
2023-02-13 13:31:18 +0100

    Update GameManager.cs

--- PWeekProject/Assets/GameManager.cs
+++ PWeekProject/Assets/GameManager.cs
@@ -1,13 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Scene currentScene = SceneManager.GetActiveScene();
+        string sceneName = currentScene.name;
+
+        if (sceneName == "main menu")
+        {
+            Debug.Log("mainmenu");
+        }
+
     }
 
     // Update is called once per frame
