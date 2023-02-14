82394f4ce00c4735e80a4efc834c67507c22e678
Okiman42 <antonaxel05@gmail.com>
2023-02-14 10:06:19 +0100

    Anton

--- PWeekProject/Assets/Scrpits/MainMenu.cs
+++ PWeekProject/Assets/Scrpits/MainMenu.cs
@@ -1,28 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
-
-
-
-
-
     // Start is called before the first frame update
     void Start()
     {
         
-
     }
 
     // Update is called once per frame
     void Update()
     {
-       
         
-    }  
+    }
 }
